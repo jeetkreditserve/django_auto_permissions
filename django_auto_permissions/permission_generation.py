5b433bac50dd1fb3e69d754a6dc56a4ad5fbdba5
Jeet Padhya <jeet@kreditserve.com>
2023-12-27 20:51:26 +0530

    Test 4

--- django_auto_permissions/permission_generation.py
+++ django_auto_permissions/permission_generation.py
@@ -1,26 +1,23 @@
-# django_auto_permissions/auto_permissions/permission_generation.py
-
-from django.contrib.auth.models import Permission
-from django.contrib.contenttypes.models import ContentType
-
 from .viewset_analysis import analyze_registered_viewsets
 
 
 def generate_permission_codename(viewset_class, method_name):
     """
-    Generates a unique permission codename for a given viewset method.
+        Generates a unique permission codename for a given viewset method.
 
-    :param viewset_class: The viewset class.
-    :param method_name: The custom method name.
-    :return: A string representing the permission codename.
-    """
+        :param viewset_class: The viewset class.
+        :param method_name: The custom method name.
+        :return: A string representing the permission codename.
+        """
     return f"{viewset_class.__name__.lower()}_{method_name}"
 
 
 def register_permissions():
     """
-    Registers generated permissions with Django's permission system.
-    """
+        Registers generated permissions with Django's permission system.
+        """
+    from django.contrib.auth.models import Permission
+    from django.contrib.contenttypes.models import ContentType
     for viewset_class, methods in analyze_registered_viewsets().items():
         for method in methods:
             codename = generate_permission_codename(viewset_class, method)
